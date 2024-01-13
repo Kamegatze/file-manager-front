66ba23d2732c00a2da89ecd0366572a699d3dabe
Shirayev Aleksey <aleksi.aleksi2014@yandex.ru>
2024-01-13 06:58:25 +0300

    Рефакторинг корневого компонета

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {LocalStorageImp} from "@utilities/local-storage/imp/local-storage-imp";
+import {Router} from "@angular/router";
+import {AuthenticationService} from "@authentication/services/authentication/authentication.service";
+import {HttpClient} from "@angular/common/http";
+import {Subscription} from "rxjs";
+import {JwtToken} from "@authentication/models/jwt-token";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  private isAuthenticationSubscription$!: Subscription;
+  constructor(private localStorage: LocalStorageImp,
+              private router: Router,
+              private authentication: AuthenticationService,
+              private http: HttpClient) {}
+
+  ngOnInit(): void {
+    const authentication = this.localStorage
+      .getValueLocalStorage<JwtToken>(this.authentication.getKeyJwtObject());
+    if (authentication === null && !this.router.url.includes("/authentication")) {
+      this.router.navigate(["authentication"]).then();
+      return;
+    }
+    this.isAuthenticationSubscription$ = this.http
+      .get<boolean>(`${this.authentication.getAuthenticationUrl()}/is-authentication`)
+      .subscribe(isAuthentication => {
+        if (!isAuthentication && !this.router.url.includes("/authentication")) {
+          this.router.navigate(["authentication"]).then();
+        }
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.isAuthenticationSubscription$?.unsubscribe();
+  }
 }
