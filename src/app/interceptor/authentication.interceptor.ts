7a0d42fd656dd84b4ac1ae6f990f261a9bb7787b
Shirayev Aleksey <aleksi.aleksi2014@yandex.ru>
2024-01-13 06:54:30 +0300

    Окончательный вариант интерцептора для http запросов

--- src/app/interceptor/authentication.interceptor.ts
+++ src/app/interceptor/authentication.interceptor.ts
@@ -1,5 +1,13 @@
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
-import {Observable} from "rxjs";
+import {
+  HttpBackend, HttpClient,
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+  HttpResponse
+} from '@angular/common/http';
+import {catchError, Observable, switchMap} from "rxjs";
 import {Injectable} from "@angular/core";
 import {AuthenticationService} from "@authentication/services/authentication/authentication.service";
 import {LocalStorageImp} from "@utilities/local-storage/imp/local-storage-imp";
@@ -10,9 +18,12 @@ import {AuthenticationHeader} from "@authentication/models/AuthenticationHeader"
 @Injectable()
 export class AuthenticationInterceptor implements HttpInterceptor {
 
+  private http!: HttpClient;
   constructor(private authentication: AuthenticationService,
               private localStorage: LocalStorageImp,
-              private router: Router,) {
+              private router: Router,
+              httpBackend: HttpBackend) {
+    this.http = new HttpClient(httpBackend);
   }
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const jwtObject = this.localStorage.getValueLocalStorage<JwtToken>(this.authentication.getKeyJwtObject());
@@ -22,16 +33,66 @@ export class AuthenticationInterceptor implements HttpInterceptor {
       }
       return next.handle(req);
     }
-    const authenticationHeader = <AuthenticationHeader>{
-      Authorization: `Bearer ${jwtObject.tokenAccess}`,
+
+    const authenticationHeader: AuthenticationHeader = <AuthenticationHeader>{
+      Authorization: jwtObject.tokenAccess !== undefined ? `Bearer ${jwtObject.tokenAccess}`: undefined,
       AuthorizationRefresh: jwtObject.refreshToken
     };
 
+    /*
+    * Проверка валидности refresh token.
+    * */
+    this.http.get<boolean>(`${this.authentication.getAuthenticationUrl()}/is-authentication`,
+      {headers: <any>authenticationHeader}).subscribe(isAuthentication => {
+      if (!isAuthentication && !this.router.url.includes("/authentication")) {
+        this.router.navigate(['authentication']).then();
+      }
+    })
+
+    const isInsertHeader = authenticationHeader.AuthorizationRefresh !== undefined
+      && authenticationHeader.Authorization !== undefined && !this.router.url.includes("/authentication");
+
     const authRequest = req.clone({
-      setHeaders: <any>authenticationHeader
+      setHeaders: isInsertHeader ? <any>authenticationHeader : {}
     });
 
-    return next.handle(authRequest);
+
+    return next.handle(authRequest).pipe(
+      catchError((err) => {
+        if (err.status === 401 && err instanceof HttpErrorResponse) {
+          console.error("Error authorization");
+          return this.updateAccessToken(`${this.authentication.getAuthenticationUrl()}/authentication`,
+            {headers: {AuthorizationRefresh: jwtObject.refreshToken}}, next, authRequest, req, authenticationHeader);
+        }
+        throw `Error request by url: ${authRequest.url}. Details: ${err.message}`;
+      })
+    );
+  }
+
+  private updateAccessToken(url: string, headers: object, next: HttpHandler,
+                            authRequest: HttpRequest<any>, req: HttpRequest<any>, authenticationHeader: AuthenticationHeader): Observable<HttpEvent<any>> {
+    return this.http.get<JwtToken>(url, headers)
+      .pipe(
+        switchMap((jwtToken) => {
+
+          if (jwtToken === null) {
+            return next.handle(authRequest);
+          }
+          if (jwtToken === undefined) {
+            return next.handle(authRequest);
+          }
+          this.localStorage.setValueLocalStorage(this.authentication.getKeyJwtObject(), jwtToken);
+          const header = <AuthenticationHeader>{
+            Authorization: jwtToken.tokenAccess !== undefined ? `Bearer ${jwtToken.tokenAccess}`: undefined,
+            AuthorizationRefresh: jwtToken.refreshToken
+          };
+          const isInsertHeaderToken = authenticationHeader.AuthorizationRefresh !== undefined &&
+            authenticationHeader.Authorization !== undefined;
+          const request = req.clone({
+            setHeaders: isInsertHeaderToken ? <any>header : {}
+          });
+          return next.handle(request);
+    }));
   }
 
 }
